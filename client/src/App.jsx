f05b0e87b9f4f0fefb280a92704f7b715e01e9f4
unknown <williamryuputra@yahoo.co.id>
2024-07-02 13:15:58 -0400

    Adds roboto as main font and adds logo as icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="bg-adjective">adj</div>
-      <div className="bg-adverb">adv</div>
+      <div className="bg-adverb ">adv</div>
       <div className="bg-conjunction">con</div>
       <div className="bg-interjection">int</div>
       <div className="bg-noun">nou</div>
