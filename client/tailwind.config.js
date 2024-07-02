e4a5b8c953e032078d60e9ab9138b9e5f48ad80d
unknown <williamryuputra@yahoo.co.id>
2024-07-02 12:17:15 -0400

    Adds tailwind and color palette for parts of speech

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        adjective:'#FDFC41',
+        adverb:'#9BFF63',
+        conjunction:'#D933FF',
+        interjection:'#FF54B0',
+        noun:'#FF5252',
+        preposition:'#59E1FF',
+        pronoun:'#FF8C5A',
+        verb:'#FFCD4C'
+      }
+    },
+  },
+  plugins: [],
+}
+
