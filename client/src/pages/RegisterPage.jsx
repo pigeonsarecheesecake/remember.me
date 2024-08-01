afa8507138c84aa40f48a32b0143dcd553036fa5
unknown <williamryuputra@yahoo.co.id>
2024-08-01 16:26:29 -0400

    Successfully creates user from client to database

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,21 +1,34 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+
 
 const RegisterPage = () => {
   const [name,setName] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit=(ev)=>{
+  const handleSubmit= async (ev)=>{
     ev.preventDefault()
+    const userData = {
+      name, username, password
+    }
+    try {
+      const res = await axios.post('/account/register', {
+        ...userData
+      })
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className="border w-full">
       <h2 className='border w-full text-center'>Create an account</h2>
       <form className='border flex flex-col items-center' onSubmit={handleSubmit}>
-        <input placeholder='name' className='w-2/4 border' value={name} onChange={(ev)=>{setName(ev.target.value)}} type="text" />
-        <input placeholder='username' className='w-2/4 border' value={username} onChange={(ev)=>{setUsername(ev.target.value)}} type="text" />
-        <input placeholder='password'className='w-2/4 border' value={password} onChange={(ev)=>{setPassword(ev.target.value)}} type="password" name="" id="" />
+        <input required placeholder='name' className='w-2/4 border' value={name} onChange={(ev)=>{setName(ev.target.value)}} type="text" />
+        <input required placeholder='username' className='w-2/4 border' value={username} onChange={(ev)=>{setUsername(ev.target.value)}} type="text" />
+        <input required placeholder='password'className='w-2/4 border' value={password} onChange={(ev)=>{setPassword(ev.target.value)}} type="password" name="" id="" />
         <button>Register</button>
       </form>
     </div>
