17b41c104efc651d5a5bae2bca543052452feba7
unknown <williamryuputra@yahoo.co.id>
2024-08-12 12:56:57 -0400

    Finishes draft for masonry layout for ResultsPage.jsx

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -5,8 +5,8 @@ import WorbiteOverlay from '../components/WorbiteOverlay'
 import { useNavigate } from 'react-router-dom'
 
 const ResultsPage = ({activeId,searchResults,setActiveWorbite}) => {
-  const [worbiteObjects, setWorbiteObjects ] = useState([])
-  const activeWorbite = worbiteObjects.find(worbiteObject => worbiteObject.id === activeId)
+  const [worbites, setWorbites ] = useState([])
+  const activeWorbite = worbites.find(worbiteObject => worbiteObject.id === activeId)
   const navigate = useNavigate()
   
   useEffect(()=>{
@@ -26,7 +26,7 @@ const ResultsPage = ({activeId,searchResults,setActiveWorbite}) => {
           backgroundColor:`bg-${fl}`
         })
       }
-      setWorbiteObjects(formattedWorbites)
+      setWorbites(formattedWorbites)
     }
     formatsObject(searchResults)
   },[searchResults]) //Function runs everytime searchResults is updated
@@ -34,16 +34,64 @@ const ResultsPage = ({activeId,searchResults,setActiveWorbite}) => {
   useEffect(()=>{
     setActiveWorbite(activeWorbite)
   },[activeId])
-  
+
+  // Worbite groups to achieve masonry layout
+  let j = 1
+  const groups = {
+    group1:[],
+    group2:[],
+    group3:[],
+    group4:[],
+    group5:[]
+  }
+  for(let i=0;i<worbites.length;i++){
+    if(j>5){
+        j=1
+    }
+    groups[`group${j}`].push(worbites[i])
+    j++
+  }
   return (
-    // <Worbite />
-    <>
-    {worbiteObjects.map(worbiteObject => (<Worbite worbiteObject={worbiteObject} key={worbiteObject.id} id={worbiteObject.id}  />)
-    )}
+    <div className=" h-[86vh] grid grid-cols-5 gap-1.5 overflow-y-scroll scrollbar-none">
+      <div className="">
+        {
+          groups.group1.map(worbiteObject=>(
+            <Worbite worbiteObject={worbiteObject} id={worbiteObject.id} key={worbiteObject.id}/>
+          ))
+        }
+      </div>
+      <div className="">
+        {
+          groups.group2.map(worbiteObject=>(
+            <Worbite worbiteObject={worbiteObject} id={worbiteObject.id} key={worbiteObject.id}/>
+          ))
+        }
+      </div>
+      <div className="">
+        {
+          groups.group3.map(worbiteObject=>(
+            <Worbite worbiteObject={worbiteObject}  id={worbiteObject.id} key={worbiteObject.id}/>
+          ))
+        }
+      </div>
+      <div className="">
+        {
+          groups.group4.map(worbiteObject=>(
+            <Worbite worbiteObject={worbiteObject} id={worbiteObject.id}key={worbiteObject.id}/>
+          ))
+        }
+      </div>
+      <div className="">
+        {
+          groups.group5.map(worbiteObject=>(
+            <Worbite worbiteObject={worbiteObject} id={worbiteObject.id} key={worbiteObject.id}/>
+          ))
+        }
+      </div>
       <DragOverlay>
         {activeId ? (<WorbiteOverlay activeWorbite={activeWorbite} />) : null}
       </DragOverlay>
-    </>
+    </div>
   )
 }
 
