018b9d48361ce93eef3b67b23fa95a269055266f
unknown <williamryuputra@yahoo.co.id>
2024-09-03 13:20:22 -0400

    Adds horizontal lines for collected worbties

--- client/src/components/CollectedWorbite.jsx
+++ client/src/components/CollectedWorbite.jsx
@@ -1,19 +1,23 @@
-import React, { useContext, useState } from 'react'
+import {parseISO, format} from 'date-fns'
 
 const CollectedWorbite = ({worbiteObject, setIsModalOpen, isModalOpen, setModal}) => {
   // Adds backgroundColor property for tailwind background class
   worbiteObject.backgroundColor = `bg-${worbiteObject.partOfSpeech}`
 
   // Worbite Object content
-  const {worbite , partOfSpeech, definition, backgroundColor,_id} = worbiteObject
+  const {worbite , partOfSpeech, definition, backgroundColor,_id, createdAt} = worbiteObject
+  const date  = parseISO(createdAt)
+  const formattedDate = format(date, 'MM/dd/yyyy')
+  
   const handleClick = () => {
     isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true)
     setModal(worbiteObject)
   }
+
   return (
     <>
     <button onClick={handleClick} className={`${backgroundColor} w-full p-3 [grid-row-start:1] [grid-column-start:1] flex flex-col justify-center rounded-[14px] mb-2 text-left `}>
-      <div className='mb-2'>
+      <div className=''>
         <p className=' break-words text-md leading-4'>
           {worbite} 
         </p>
@@ -21,9 +25,11 @@ const CollectedWorbite = ({worbiteObject, setIsModalOpen, isModalOpen, setModal}
           {partOfSpeech}
         </p>
       </div>
+      <div className='border-b border border-black w-full my-2'></div>
       <p className='font-normal text-[0.70rem] leading-3'>
         {definition}
       </p>
+      <p className='text-[8px] font-normal mt-2 text-right w-full'>{formattedDate}</p>
     </button>
     </>
   )
